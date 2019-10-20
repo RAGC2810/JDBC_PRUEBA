27542b7d430d78afe9b43b5787b8996839576a0c
elquetuquieras <elquetuquieras>
2019-10-19 23:47:21 -0500

    nuevo commit

--- src/main/java/datos/PersonaJDBC.java
+++ src/main/java/datos/PersonaJDBC.java
@@ -8,7 +8,7 @@ import java.util.List;
 public class PersonaJDBC {
 
     private Connection conexionTransaccional;
-//comentario3333
+//comenta
     private static final String SQL_SELECT = "SELECT id_persona, nombre, apellido, email, telefono FROM persona";
     private static final String SQL_INSERT = "INSERT INTO persona(nombre, apellido, email, telefono) VALUES(?, ?, ?, ?)";
     private static final String SQL_UPDATE = "UPDATE persona SET nombre=?, apellido=?, email=?, telefono=? WHERE id_persona = ?";
